61ade32a5d9c40caa3befaaa449018a249c59b40
TanKARASU <karasutan@gmail.com>
2021-03-20 11:56:49 +0100

    Router implemented needs links

--- src/App.js
+++ src/App.js
@@ -1,8 +1,50 @@
-import logo from "./logo.svg";
-import "./App.css";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+} from "react-router-dom";
+
+// component import
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
-  return <div className="App">Test</div>;
+  return (
+    <Router>
+      <div>
+        <h2>Welcome</h2>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <ul className="navbar-nav mr-auto">
+            <li>
+              <Link to={"/"} className="nav-link">
+                {" "}
+                Home{" "}
+              </Link>
+            </li>
+            <li>
+              <Link to={"/contact"} className="nav-link">
+                Contact
+              </Link>
+            </li>
+            <li>
+              <Link to={"/about"} className="nav-link">
+                About
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <hr />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
